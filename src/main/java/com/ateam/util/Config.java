35d9ad20a29c6a13eb3187c801173b93df7e6b39
rkushmiruk <rkushmiruk@gmail.com>
2017-12-26 22:44:18 +0200

    Fix config

--- src/main/java/com/ateam/util/Config.java
+++ src/main/java/com/ateam/util/Config.java
@@ -3,15 +3,11 @@ package com.ateam.util;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.node.NodeBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
-@EnableElasticsearchRepositories(basePackages = "com.baeldung.spring.data.es.repository")
-@ComponentScan(basePackages = {"com.baeldung.spring.data.es.service"})
 public class Config {
 
     @Bean
