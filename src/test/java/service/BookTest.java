7c58ee2fc0a1f60c6462faf3787eb64c33e8b131
Staff2708 <Staff2708@yandex.ru>
2017-12-26 20:03:18 +0200

    Create new BookTest

--- src/test/java/service/BookTest.java
+++ src/test/java/service/BookTest.java
@@ -3,8 +3,8 @@ package service;
 import com.ateam.entity.Book;
 import com.ateam.repository.BookRepository;
 import com.ateam.service.BookService;
+import com.google.common.collect.ImmutableList;
 
-import org.h2.engine.Session;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,7 +13,16 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
@@ -30,9 +39,41 @@ public class BookTest {
         bookRepository = mock(BookRepository.class);
     }
 
-    @Test(expected = org.springframework.orm.jpa.JpaSystemException.class)
     public void saveNullAuthor() {
-        Book book = new Book();
+        Book book = new Book("12345567", "New", "Roman", "yes");
         bookService.save(book);
     }
+
+    @Test
+    public void getAllBookTest() {
+        Book book = new Book("12345", "New", "Roman", "yes");
+        List<Book> books = new ArrayList<>();
+        books.add(book);
+
+        when(bookRepository.findAll()).thenReturn(books);
+        List<Book> result = ImmutableList.copyOf(bookRepository.findAll());
+        verify(bookRepository, atLeastOnce()).findAll();
+
+        long expectedValue = 1L;
+        long actualValue = result.size();
+        assertThat(expectedValue, is(actualValue));
+    }
+
+    @Test
+    public void findBookTest() {
+        Book book = bookService.findById(1L);
+
+        assertNotNull(book);
+        assertThat(book.getIsbn(), is("123"));
+        assertThat(book.getPublisher(), is("New Times"));
+        assertThat(book.getTitle(), is("King"));
+        assertThat(book.getAvailability(), is("yes"));
+    }
+
+    @Test
+    public void findAllBooksTest() {
+        List<Book> books = bookService.findAll();
+        assertThat(books.size(), is(3));
+    }
+
 }
