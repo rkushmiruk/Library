3cbb671efb2d4908acd31cfd665522b973c10a1c
rkushmiruk <rkushmiruk@gmail.com>
2017-12-26 22:41:31 +0200

    Add Spring retry and elasticSearch config

--- src/main/java/com/ateam/service/AuthorService.java
+++ src/main/java/com/ateam/service/AuthorService.java
@@ -1,8 +1,20 @@
 package com.ateam.service;
 
 
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.transaction.CannotCreateTransactionException;
+
 import com.ateam.entity.Author;
 
+@EnableRetry
 public interface AuthorService extends GenericService<Author> {
+
+    @Retryable(
+        value = CannotCreateTransactionException.class,
+        backoff = @Backoff(delay = 5000,
+            maxDelay = 100000,
+            multiplier = 2))
     Long countAll();
 }
--- src/main/java/com/ateam/service/GenericService.java
+++ src/main/java/com/ateam/service/GenericService.java
@@ -1,13 +1,42 @@
 package com.ateam.service;
 
+import java.sql.SQLException;
 import java.util.List;
 
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.transaction.CannotCreateTransactionException;
+
+@EnableRetry
 public interface GenericService<T>  {
+    @Retryable(
+        value = CannotCreateTransactionException.class,
+        backoff = @Backoff(delay = 5000,
+            maxDelay = 100000,
+            multiplier = 2))
     List<T> findAll();
 
+    @Retryable(
+        value = CannotCreateTransactionException.class,
+        backoff = @Backoff(delay = 5000,
+            maxDelay = 100000,
+            multiplier = 2))
     T findById(Long id);
 
+    @Retryable(
+        value = CannotCreateTransactionException.class,
+        backoff = @Backoff(delay = 5000,
+            maxDelay = 100000,
+            multiplier = 2))
     T save(T entity);
 
+    @Retryable(
+        value = CannotCreateTransactionException.class,
+        backoff = @Backoff(delay = 5000,
+            maxDelay = 100000,
+            multiplier = 2))
     void delete(Long id);
+
 }
